ea5e39bf4df7fa730c168060ea69cec9e4ab3898
Agustin Mista <agustin@mista.me>
2025-11-26 22:20:00 +0100

    Add copilot CLI derivation

---
+++ copilot/default.nix
@@ -0,0 +1,30 @@
+{
+  stdenv,
+  fetchurl,
+  nodejs,
+}:
+stdenv.mkDerivation rec {
+  pname = "copilot";
+  version = "0.0.365";
+  src = fetchurl {
+    url = "https://registry.npmjs.org/@github%2fcopilot/-/copilot-${version}.tgz";
+    hash = "sha256-T1dbgFGbhjZG2AXpPodOTiFEBuD5YhzmimD14NY87KU=";
+  };
+  buildInputs = [ nodejs ];
+  dontBuild = true;
+  unpackPhase = ''
+    tar -xf $src # contains package/
+    ls -la
+  '';
+  installPhase = ''
+    mkdir -p $out/lib
+    cp -r package/* $out/lib/
+
+    mkdir -p $out/bin
+    cat > $out/bin/copilot <<EOF
+    #!/bin/sh
+    exec ${nodejs}/bin/node $out/lib/index.js "\$@"
+    EOF
+    chmod +x $out/bin/copilot
+  '';
+}
